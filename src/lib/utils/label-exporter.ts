1c227e5f15730a7c79bcc6839e66d43e9f75b826
Kamil Pająk <kamilpajak@users.noreply.github.com>
2026-07-01 14:12:24 +0200

    refactor: use Element.remove() instead of parentNode.removeChild()

--- src/lib/utils/label-exporter.ts
+++ src/lib/utils/label-exporter.ts
@@ -197,7 +197,7 @@ function downloadCanvasAsPng(canvas: HTMLCanvasElement, filename: string): Promi
 			// Use requestAnimationFrame to ensure the link is in the DOM and browser is ready
 			requestAnimationFrame(() => {
 				a.click();
-				document.body.removeChild(a);
+				a.remove();
 				URL.revokeObjectURL(url);
 				console.log('Download link clicked and cleaned up');
 				resolve();
